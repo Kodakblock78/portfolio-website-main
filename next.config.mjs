1cdc00aa59c0d2e7370c4aca9a8eaa6955c09066
ezekiel <ezekielforko875@gmail.com>
2025-06-11 10:52:04 +0000

    fixed next config hopefully fixing the issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Optional: If you're deploying to a subpath (e.g., your GitHub repo name), set this:
-    basePath: '/portfolio-website-main', // Replace 'portfolio-website-main' with your actual repo name
-  
-    // Use output: 'export' to enable static export in Next.js
-    output: 'export',
-  
-    // Optional: Disable image optimization for static export
-    images: {
-      unoptimized: true,
-    },
-  
-    // Enable React Strict Mode (optional, for development)
-    reactStrictMode: true,
+
   };
   
   export default nextConfig;
